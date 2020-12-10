df788c80660c5bb83848d6a8c60874ab7f672e50
Luis Huerta <lehuertad95@gmail.com>
2020-12-10 10:06:37 -0400

    Improved styling and fixed active class for navlinks bug

--- src/components/layout/NavbarLink.js
+++ src/components/layout/NavbarLink.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 export const NavbarLink = ({ to, text, icon }) => (
 	<li className="NavbarLink">
-		<NavLink exact to={to} activeClassName="active">
+		<NavLink exact={to === '/'} to={to} activeClassName="active">
 			<i className={icon}></i> <span>{text}</span>
 		</NavLink>
 	</li>
